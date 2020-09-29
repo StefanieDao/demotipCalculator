dc30d9b02f7faf4afc72e7f51339422a5a70e7ef
Stefanie Dao <ctdao@ucsd.edu>
2020-09-29 09:32:50 -0700

    Update ViewController.swift
    Update after accidentally deleting the file

--- tip calculator/ViewController.swift
+++ tip calculator/ViewController.swift
@@ -10,19 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBOutlet weak var billField: UITextField!
-    @IBOutlet weak var tipControl: UISegmentedControl!
     
-    @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var tipLabel: UILabel!
-    @IBAction func onTap(_ sender: Any) {
-        view.endEditing(true)
-    }
     @IBAction func calculateTip(_ sender: Any) {
         let bill = Double(billField.text!) ?? 0
         
@@ -30,8 +31,8 @@ class ViewController: UIViewController {
         let tip = bill * tipPercent[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
-        tipLabel.text = String(format: "%$.2f", tip)
-        totalLabel.text = String(format: "%$.2f", total)
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
 
     }
 }
